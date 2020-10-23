6779e68b31521257902c6cd890b43fee421e6ca6
007javac <yurakaramyshev@yandex.com>
2020-10-24 00:52:24 +0200

    7. Date. Отображение даты. [#310017]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -5,7 +5,7 @@ import java.time.format.DateTimeFormatter;
 public class StartUI {
     public static void main(String[] args) {
         Item item = new Item();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss ");
         String itemCreatedDateFormat = item.getCreated().format(formatter);
         System.out.println(itemCreatedDateFormat);
     }
